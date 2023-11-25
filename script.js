1bec6fae2d0cc951252df7c3d578f6cf733cf006
ujuanzinn <130804331+ujuanzinn@users.noreply.github.com>
2023-11-24 21:48:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -75,11 +75,11 @@ function removerFilme(index) {
   listaFilmesFavoritos.splice(index - 1, 1);
   recarregarFilmes();
 }
-
-function updateDateTime() {
-            const now = new Date();
-            const dateTimeString = now.toLocaleDateString() + ' ' + now.toLocaleTimeString();
-            document.getElementById('current-date-time').innerText = dateTimeString;
-        }
+function updateDateTime() 
+{
+  const now = new Date();
+  const dateTimeString = now.toLocaleDateString() + ' ' + now.toLocaleTimeString();
+  document.getElementById('current-date-time').innerText = dateTimeString;
+}
 
 setInterval(updateDateTime, 1000);
